272f3c4564bb4a7ec0cb7001ccc6164a3be750bf
hasansharif819 <hs.sharif819@gmail.com>
2025-02-26 01:22:06 +0600

    initial commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true, // Set to false in production
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
